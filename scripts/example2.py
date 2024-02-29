fe951df179ad4d67ee634bbba7caa13f7e06df30
Nico Spiller <nspiller1@mailbox.org>
2024-02-29 15:35:07 -0500

    added second notebook

---
+++ scripts/example2.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: everything
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Title
+# We need a second script file for testing purposes
+
+# %%
+print('Hello world!')
